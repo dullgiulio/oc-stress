f6bda6cda0a2b447755a18e4429bf850258b313d
Giulio Iotti <dullgiulio@gmail.com>
2018-03-13 15:30:29 +0000

    add process output matcher

--- proc.go
+++ proc.go
@@ -3,9 +3,98 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
+	"log"
 	"os/exec"
 )
+
+var errRunRetry = errors.New("retry")
+
+type retryFn func(r io.Reader) error
+
+func ocScale(dc string, replicas int) error {
+	// TODO: oc scale --replicas=$replicas $dc
+	//		 runUntil(oc get dc/$dc, ocStatusIsDesired(dc), 1 second)
+}
+
+func ocLogs(dc string, m *matcher) error {
+	// TODO: oc logs -f dc/$dc
+}
+
+func ocStatusIsDesired(pod string) retryFn {
+	return func(r io.Reader) error {
+		sc := bufio.NewScanner(r)
+		sc.Scan()
+		if err := sc.Err(); err != nil {
+			return fmt.Errorf("cannot read command output: %v", err)
+		}
+		for sc.Scan() {
+			fields := split(sc.Bytes())
+			if len(fields) != 5 {
+				return fmt.Errorf("expected 5 fields in line '%s'", sc.Text())
+			}
+			if fields[0] != pod {
+				continue
+			}
+			if fields[2] != fields[3] {
+				return errRunRetry
+			}
+			return nil
+		}
+		if err := sc.Err(); err != nil {
+			return fmt.Errorf("cannot read command output: %v", err)
+		}
+		return errRunRetry
+	}
+}
+
+func split(line []byte) []string {
+	var i, j int
+	words := make([]string, 0)
+	for ; i < len(line); i++ {
+		if !isspace(line[i]) {
+			continue
+		}
+		if i <= j {
+			continue
+		}
+		words = append(words, string(line[j:i]))
+		for ; i < len(line); i++ {
+			if !isspace(line[i]) {
+				break
+			}
+		}
+		j = i
+	}
+	if j < i {
+		words = append(words, string(line[j:i]))
+	}
+	return words
+}
+
+func isspace(b byte) bool {
+	return b == ' ' || b == '\t' || b == '\v' || b == '\n'
+}
+
+func runUntil(cmd *exec.Cmd, fn retryFn, sleep time.Duration) error {
+	for {
+		out, err := cmd.Output()
+		if err != nil {
+			return err
+		}
+		err = fn(bytes.NewReader(out))
+		if err == nil {
+			return nil
+		}
+		if err != errRunRetry {
+			return err
+		}
+		time.Sleep(sleep)
+	}
+	return nil
+}
 
 type mstatus struct {
 	nlines  int64
@@ -16,7 +105,7 @@ type mstatus struct {
 type matcher struct {
 	reader io.Reader
 	match  [][]byte
-	update chan *status
+	update chan *mstatus
 	status *mstatus
 }
 
@@ -28,7 +117,7 @@ func newMatcher(r io.Reader, match []string) *matcher {
 	return &matcher{
 		reader: r,
 		match:  bms,
-		update: make(chan *status),
+		update: make(chan *mstatus),
 	}
 }
 
@@ -38,7 +127,7 @@ func (m *matcher) run() {
 		m.status.matches += st.matches
 		m.status.err = st.err
 		if st.err != nil {
-			log.Printf("[error] %v", err) // TODO: context etc
+			log.Printf("[error] %v", st.err) // TODO: context etc
 		}
 	}
 }
@@ -52,11 +141,11 @@ func (m *matcher) matches(bs []byte) bool {
 	return false
 }
 
-func (m *matcher) slurp(updateEvery int) {
+func (m *matcher) slurp(updateEvery int64) {
 	sc := bufio.NewScanner(m.reader)
 	st := &mstatus{}
-	for scanner.Scan() {
-		if m.matches(scanner.Bytes()) {
+	for sc.Scan() {
+		if m.matches(sc.Bytes()) {
 			st.matches++
 		}
 		st.nlines++
@@ -67,8 +156,9 @@ func (m *matcher) slurp(updateEvery int) {
 			st = &mstatus{}
 		}
 	}
-	if err := scanner.Err(); err != nil {
+	if err := sc.Err(); err != nil {
 		st.err = err
 	}
 	m.update <- st
+	close(m.update)
 }
---
+++ proc_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestSplitOutputLine(t *testing.T) {
+	line := []byte("test-shutdown-receiver   36         0         1         config,image(test-shutdown-receiver:latest)")
+	expected := []string{"test-shutdown-receiver", "36", "0", "1", "config,image(test-shutdown-receiver:latest)"}
+	words := split(line)
+	if len(words) != len(expected) {
+		t.Fatalf("expected %d words, got %d (%+v)", len(expected), len(words), words)
+	}
+	for i := range words {
+		if words[i] != expected[i] {
+			t.Errorf("word %d is '%s', expected '%s'", i, words[i], expected[i])
+		}
+	}
+}
+
+func TestOcStatusIsDesired(t *testing.T) {
+	output := []byte(`NAME                     REVISION   DESIRED   CURRENT   TRIGGERED BY
+test-shutdown-receiver   36         0         1         config,image(test-shutdown-receiver:latest)
+`)
+	testFn := ocStatusIsDesired("test-shutdown-receiver")
+	err := testFn(bytes.NewReader(output))
+	if err != errRunRetry {
+		t.Errorf("got error '%v' instead of errRunRetry", err)
+	}
+	output = []byte(`NAME                     REVISION   DESIRED   CURRENT   TRIGGERED BY
+test-shutdown-receiver   36         0         0         config,image(test-shutdown-receiver:latest)
+`)
+	err = testFn(bytes.NewReader(output))
+	if err != nil {
+		t.Errorf("got error '%v' instead of nil", err)
+	}
+	testFn = ocStatusIsDesired("test")
+	err = testFn(bytes.NewReader(output))
+	if err != errRunRetry {
+		t.Errorf("got error '%v' instead of errRunRetry", err)
+	}
+}
